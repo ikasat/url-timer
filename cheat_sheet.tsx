514b62e4079de4e6d71069cd672acf0e2f33424a
ikasat <s.tasaki@asahinet.com>
2018-08-24 20:43:41 +0900

    refactor

--- cheat_sheet.tsx
+++ cheat_sheet.tsx
@@ -13,56 +13,57 @@ import * as Reselect from "reselect";
 import * as History from "history";
 
 // 時刻は moment、時間は moment-duration-format で扱う
+// compilerOptions (tsconfig.json) の allowSyntheticDefaultImports を true にする必要あり
 import moment from "moment";
 import momentDurationFormat from "moment-duration-format";
 momentDurationFormat(moment);
 
-// 環境設定
-const baseURLPath = "/";
+// ■ 環境設定
 
-// ユーティリティ関数
+const baseURLPath = "/url-timer/";
+
+// ■ ユーティリティ関数
+
 const defaultString = (s: string | null | undefined, defaultS: string = "") => (s == null ? defaultS : s);
-const isBlank = (s: string | null | undefined) => (s == null || s === "");
-function nullablify<T, R>(f: (x: T) => R): (x: T | undefined) => R | undefined {
-  return (x: T | undefined) => (x == null ? void(0) : f(x));
-}
-const decodeURIComponentIfNotNull = nullablify(decodeURIComponent);
+const isBlank = (s: string | null | undefined) => s == null || s === "";
 const timeStringToTimestamp = (s: string): number | undefined => {
   const m = moment(s, [moment.ISO_8601, "HH:mm", "X"]);
-  const timestamp = parseInt(s);
-  if (m.isValid()) {
-    return m.unix();
-  } else {
-    return isNaN(timestamp) ? void(0) : timestamp;
-  }
-}
+  return m.isValid() ? m.unix() : void 0;
+};
 
-// State
-type State = {
-  timer: TimerState;
-  form: ReduxForm.FormStateMap;
-  router: ConnectedReactRouter.RouterState;
-}
+// ■ State
 
 type TimerState = {
   nowTimestamp: number;
   targetTimeString?: string;
   targetTimestamp?: number;
-}
+};
 
 const initialTimerState: TimerState = {
   nowTimestamp: moment().unix()
 };
 
-// Action
+type State = {
+  timer: TimerState;
+  form: ReduxForm.FormStateMap;
+  router: ConnectedReactRouter.RouterState;
+};
+
+// ■ Action
+
 const actionCreator = TypeScriptFSA.actionCreatorFactory();
 const changeNowTimestamp = actionCreator<number>("timer/CHANGE_NOW_TIMESTAMP");
-const changeTargetTime = actionCreator<{string?: string, timestamp?: number}>("timer/CHANGE_TARGET_TIME");
+const changeTargetTime = actionCreator<{ string?: string; timestamp?: number }>("timer/CHANGE_TARGET_TIME");
 
-// Reducer
+// ■ Reducer
+
 const timerReducer = TypeScriptFSAReducers.reducerWithInitialState(initialTimerState)
   .case(changeNowTimestamp, (timerState, payload) => ({ ...timerState, nowTimestamp: payload }))
-  .case(changeTargetTime, (timerState, { string, timestamp }) => ({ ...timerState, targetTimeString: string, targetTimestamp: timestamp }))
+  .case(changeTargetTime, (timerState, { string, timestamp }) => ({
+    ...timerState,
+    targetTimeString: string,
+    targetTimestamp: timestamp
+  }))
   .build();
 
 const reducer = Redux.combineReducers({
@@ -70,10 +71,11 @@ const reducer = Redux.combineReducers({
   form: ReduxForm.reducer
 });
 
-// Store
+// ■ Store
+
 const browserHistory = History.createBrowserHistory({ basename: baseURLPath });
 
-// Redux Devtools を使う
+// Redux Devtools を使う場合
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || Redux.compose;
 
 const store = Redux.createStore(
@@ -81,14 +83,14 @@ const store = Redux.createStore(
   composeEnhancers(Redux.applyMiddleware(ConnectedReactRouter.routerMiddleware(browserHistory)))
 );
 
-// Props 定義
+// ■ Props
 
-// URL (Path) に含まれるパラメータ (react-router)
+// URL (Path) から取得するパラメータ (react-router)
 interface PathParams {
   targetTimeString?: string;
 }
 
-// form 要素を使ったパラメータ (redux-form)
+// form から取得するパラメータ (redux-form)
 interface FormData {
   targetTimeString?: string;
 }
@@ -98,7 +100,7 @@ interface OwnProps extends ReduxForm.InjectedFormProps<FormData, {}>, ReactRoute
 
 // mapStateToProps が生成する Props
 interface StateProps {
-  initialValues?: FormData;  // redux-form が使う
+  initialValues?: FormData; // redux-form が使う
   nowTimestamp: number;
   nowTimeString: string;
   targetTimestamp?: number;
@@ -117,13 +119,67 @@ interface DispatchProps {
 // mergeProps（あれば）が生成する Props
 type Props = OwnProps & StateProps & DispatchProps;
 
+// ■ Component
+
+const TimerComponent = (props: Props) => {
+  const {
+    handleSubmit,
+    onSubmit,
+    nowTimestamp,
+    nowTimeString,
+    targetTimestamp,
+    targetTimeString,
+    targetTimeISOString,
+    duration,
+    durationString
+  } = props;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <p>
+        (Now: {nowTimeString}, {nowTimestamp})
+      </p>
+      {targetTimeString == null ? (
+        <React.Fragment />
+      ) : (
+        <React.Fragment>
+          <h1>
+            {targetTimeString}
+            <br />
+            <span style={{ fontSize: "50%" }}>
+              ({targetTimeISOString}, {targetTimestamp})
+            </span>
+          </h1>
+          <h2>
+            {defaultString(durationString)} ({duration})
+          </h2>
+        </React.Fragment>
+      )}
+      <form>
+        <ul style={{ listStyleType: "none", padding: "0" }}>
+          <li>
+            <ReduxForm.Field component="input" type="text" name="targetTimeString" />
+          </li>
+          <li>
+            <button onClick={handleSubmit(onSubmit)}>Create New Timer</button>
+          </li>
+          <li>
+            <ReactRouterDOM.Link to="/">Reset</ReactRouterDOM.Link>
+          </li>
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+// ■ Container
+
 // state からデータを抽出するセレクタ
 const selectLocation = (state: State) => state.router.location;
 const selectNowTimestamp = (state: State) => state.timer.nowTimestamp;
 const selectTargetTimeString = (state: State) => state.timer.targetTimeString;
 const selectTargetTimestamp = (state: State) => state.timer.targetTimestamp;
 
-// state 中の関心のある値が更新された場合のみ新しい StateProps を生成する (reselect)
+// state 中の関心のある値が更新された場合にのみ新しい StateProps を生成する (reselect)
 const reselectProps = Reselect.createSelector(
   [selectLocation, selectNowTimestamp, selectTargetTimeString, selectTargetTimestamp],
   (_location, nowTimestamp, targetTimeString, targetTimestamp): StateProps => {
@@ -139,14 +195,14 @@ const reselectProps = Reselect.createSelector(
     const duration = targetTimestamp - nowTimestamp;
     const durationString = moment.duration(duration * 1000).format("d[d]hh:mm:ss");
     return {
-      initialValues: { },
+      initialValues: {},
       nowTimestamp,
       nowTimeString,
       targetTimestamp,
       targetTimeString,
       targetTimeISOString,
       duration,
-      durationString,
+      durationString
     };
   }
 );
@@ -167,57 +223,19 @@ const mapDispatchToProps = (dispatch: Redux.Dispatch): DispatchProps => ({
   }
 });
 
-// Component
-const TimerComponent = (props: Props) => {
-  const { handleSubmit, onSubmit, nowTimestamp, nowTimeString, targetTimestamp, targetTimeString, targetTimeISOString, duration, durationString } = props;
-  return (
-    <div style={{textAlign: "center"}}>
-      <p>(Now: { nowTimeString }, { nowTimestamp })</p>
-      {
-        targetTimeString == null
-          ? <React.Fragment />
-          : (
-            <React.Fragment>
-              <h1>
-                {targetTimeString}<br />
-                <span style={{fontSize: "50%"}}>({targetTimeISOString}, {targetTimestamp})</span>
-              </h1>
-              <h2>
-                {defaultString(durationString)} ({duration})
-              </h2>
-            </React.Fragment>
-          )
-      }
-      <form>
-        <ul style={{listStyleType: "none", padding: "0"}}>
-          <li>
-            <ReduxForm.Field component="input" type="text" name="targetTimeString" />
-          </li>
-          <li>
-            <button onClick={handleSubmit(onSubmit)}>Create New Timer</button>
-          </li>
-          <li>
-            <ReactRouterDOM.Link to="/">Reset</ReactRouterDOM.Link>
-          </li>
-        </ul>
-      </form>
-    </div>
-  );
-};
-
 // setInterval の wrapper
 const generateTicker = (handler: () => void, interval: number) => {
   return {
-    timerId: void(0) as number | undefined,
+    timerId: void 0 as number | undefined,
     counter: 0,
-    start () {
+    start() {
       if (this.counter === 0) {
         handler();
         this.timerId = setInterval(handler, interval) as any;
       }
       ++this.counter;
     },
-    stop () {
+    stop() {
       --this.counter;
       if (this.counter === 0) {
         clearInterval(this.timerId);
@@ -227,40 +245,40 @@ const generateTicker = (handler: () => void, interval: number) => {
   };
 };
 // 0.5s 毎に changeNowTimestamp Action を発行するタイマー
-const ticker = generateTicker(() => { store.dispatch(changeNowTimestamp(moment().unix())) }, 500);
+const nowTimestampTicker = generateTicker(() => {
+  store.dispatch(changeNowTimestamp(moment().unix()));
+}, 500);
 
 // URL が変更された場合の挙動
 const onRoute = Reselect.createSelector(
-  [
-    (props: Props) => props.onChangeTargetTimeString,
-    (props: Props) => props.match.params.targetTimeString
-  ],
+  [(props: Props) => props.onChangeTargetTimeString, (props: Props) => props.match.params.targetTimeString],
   (onChangeTargetTimeString, targetTimeString) => {
-    onChangeTargetTimeString(decodeURIComponentIfNotNull(targetTimeString));
+    onChangeTargetTimeString(targetTimeString != null ? decodeURIComponent(targetTimeString) : void 0);
   }
 );
 
-// Container
 const TimerContainer = Redux.compose<() => JSX.Element>(
   ReactRedux.connect(
     mapStateToProps,
-    mapDispatchToProps,
+    mapDispatchToProps
   ),
   ReduxForm.reduxForm({ form: "timerContainer" }),
   ReactRouterDOM.withRouter,
   Recompose.lifecycle<Props, State>({
     componentWillMount() {
       onRoute(this.props);
-      ticker.start();
+      nowTimestampTicker.start();
     },
     componentWillUnmount() {
-      ticker.stop();
+      nowTimestampTicker.stop();
     },
     componentWillReceiveProps(props: Props) {
       onRoute(props);
     }
-  }),
+  })
 )(TimerComponent);
+
+// ■ Application
 
 const App = () => (
   <ReactRedux.Provider store={store}>
